7cca0060a2436a3b0e0f3a63d53a9104725af1d4
Jzweifel15 <jzweifel15@yahoo.com>
2020-09-20 18:15:04 -0400

    Small Update
    Added a couple of comments of code on how he could have optimized our routes file, but kept what we originally implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'appointments#index'
 
+  # resources :appointments, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   get '/appointments', to: 'appointments#index'
   get 'appointments/new', to: 'appointments#new', as: 'create_appointment'
   post 'appointments', to: 'appointments#create'
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   patch '/appointments/:id', to: 'appointments#update'
   delete 'appointments/:id', to: 'appointments#destroy'
 
+  # resources :doctors, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   get '/doctors', to: 'doctors#index', as: 'doctors'
   get '/doctors/new', to: 'doctors#new', as: 'new_doctor'
   post '/doctors', to: 'doctors#create'
