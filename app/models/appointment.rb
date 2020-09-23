867d6a979e83e424c92ca52ad2ac1696d6a41c19
ianhutson <ih00209@georgiasouthern.edu>
2020-09-23 17:15:37 -0400

    views have validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,15 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
-  belongs_to :patient
+  # belongs_to :patient
+
+  validates :location, presence: true
+  validates :time, presence: true
+  validate :in_past
+
+  def in_past 
+    if time.past?
+      errors.add(:time, "Time must be in the future.")
+    end
+  end
+
 end
